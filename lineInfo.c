2be4e10db785bce4cea2c54d4522de3045065ba4
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-24 16:43:19 -0700

    Improving dynamic array resizing for lineInfo.

--- lineInfo.c
+++ lineInfo.c
@@ -4,6 +4,7 @@
 #include "lineInfo.h"
 
 void initLineInfo(LineInfo* lineInfo) {
+	lineInfo->capacity = 0;
 	lineInfo->size = 0;
 	lineInfo->lines = NULL;
 	lineInfo->repeats = NULL;
@@ -13,8 +14,12 @@ void writeLineInfo(LineInfo* lineInfo, int line) {
 	if (lineInfo->size > 0 && line == lineInfo->lines[lineInfo->size - 1]) {
 		lineInfo->repeats[lineInfo->size - 1]++;
 	} else {
-		lineInfo->lines = GROW_ARRAY(int, lineInfo->lines, lineInfo->size, lineInfo->size + 1);
-		lineInfo->repeats = GROW_ARRAY(int, lineInfo->repeats, lineInfo->size, lineInfo->size + 1);
+		if (lineInfo->capacity < lineInfo->size + 1) {
+			int oldCapacity = lineInfo->capacity;
+			lineInfo->capacity = GROW_CAPACITY(oldCapacity);
+			lineInfo->lines = GROW_ARRAY(int, lineInfo->lines, oldCapacity, lineInfo->capacity);
+			lineInfo->repeats = GROW_ARRAY(int, lineInfo->repeats, oldCapacity, lineInfo->capacity);
+		} 
 		lineInfo->lines[lineInfo->size] = line;
 		lineInfo->repeats[lineInfo->size] = 1;
 		lineInfo->size++;
