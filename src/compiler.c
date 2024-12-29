45577da745b7df1d334760d1d4dc10850beb5f3f
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-29 07:53:13 -0700

    Handling Logical and.

--- src/compiler.c
+++ src/compiler.c
@@ -268,6 +268,15 @@ static void defineVariable(uint8_t global) {
 	emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
+static void and_(bool canAssign) {
+	int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+	emitByte(OP_POP);
+	parsePrecedence(PREC_AND);
+
+	patchJump(endJump);
+}
+
 static void binary(bool canAssign) {
 	TokenType operatorType = parser.previous.type;
 	ParseRule* rule = getRule(operatorType);
@@ -373,7 +382,7 @@ ParseRule rules[] = {
 	[TOKEN_IDENTIFIER]	  =	{variable, NULL,    PREC_NONE},
 	[TOKEN_STRING]		  =	{string,   NULL,    PREC_NONE},
 	[TOKEN_NUMBER]		  =	{number,   NULL,    PREC_NONE},
-	[TOKEN_AND]			  =	{NULL,     NULL,    PREC_NONE},
+	[TOKEN_AND]			  =	{NULL,     and_,    PREC_AND},
 	[TOKEN_CLASS]		  =	{NULL,     NULL,    PREC_NONE},
 	[TOKEN_ELSE]		  =	{NULL,     NULL,    PREC_NONE},
 	[TOKEN_FALSE]		  =	{literal,  NULL,    PREC_NONE},
