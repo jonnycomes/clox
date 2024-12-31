d6ab2f765df2a1ae3da22b3e5e189dc44cb627cd
Jonny Comes <jcomes@collegeofidaho.edu>
2024-12-31 09:35:04 -0700

    Flattening upvalues.

--- src/compiler.c
+++ src/compiler.c
@@ -285,6 +285,11 @@ static int resolveUpValue(Compiler* compiler, Token* name) {
 		return addUpvalue(compiler, (uint8_t)local, true);
 	}
 
+	int upvalue = resolveUpValue(compiler->enclosing, name);
+	if (upvalue != -1) {
+		return addUpvalue(compiler, (uint8_t)upvalue, false);
+	}
+
 	return -1;
 }
 
@@ -566,6 +571,11 @@ static void function(FunctionType type) {
 
 	ObjFunction* function = endCompiler();
 	emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+	for (int i = 0; i < function->upvalueCount; i++) {
+		emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+		emitByte(compiler.upvalues[i].index);
+	}
 }
 
 static void funDeclaration() {
