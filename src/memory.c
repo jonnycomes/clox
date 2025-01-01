e6ac2c87aff257e64e33ea0ed3303d082ed36782
Jonny Comes <jcomes@collegeofidaho.edu>
2025-01-01 14:19:51 -0700

    GC: marking less obvious roots.

--- src/memory.c
+++ src/memory.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
+#include "compiler.h"
 #include "memory.h"
 #include "vm.h"
 
@@ -81,7 +82,16 @@ static void markroots() {
 		markValue(*slot);
 	}
 
+	for (int i = 0; i < vm.frameCount; i++) {
+		markObject((Obj*)vm.frames[i].closure);
+	}
+
+	for (ObjUpvalue* upvalue = vm.openUpvalues; upvalue != NULL; upvalue = upvalue->next) {
+		markObject((Obj*)upvalue);
+	}
+
 	markTable(&vm.globals);
+	markCompilerRoots();
 }
 
 void collectGarbage() {
