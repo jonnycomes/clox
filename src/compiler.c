b9dbe71bf5729be7e316a36ec3ca7b5f9b1e8f51
Jonny Comes <jcomes@collegeofidaho.edu>
2025-01-04 09:30:01 -0700

    Handling misusing 'this'.

--- src/compiler.c
+++ src/compiler.c
@@ -67,8 +67,13 @@ typedef struct Compiler {
 	int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+	struct ClassCompiler* enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler* current = NULL;
+ClassCompiler* currentClass = NULL;
 
 static Chunk* currentChunk() {
 	return &current->function->chunk;
@@ -484,6 +489,11 @@ static void variable(bool canAssign) {
 }
 
 static void this_(bool canAssign) {
+	if (currentClass == NULL) {
+		error("Can't use 'this' outside of a class.");
+		return;
+	}
+
 	variable(false);
 }
 
@@ -628,6 +638,10 @@ static void classDeclaration() {
 	emitBytes(OP_CLASS, nameConstant);
 	defineVariable(nameConstant);
 
+	ClassCompiler classCompiler;
+	classCompiler.enclosing = currentClass;
+	currentClass = &classCompiler;
+
 	namedVariable(className, false);
 	consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
 	while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
@@ -635,6 +649,8 @@ static void classDeclaration() {
 	}
 	consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
 	emitByte(OP_POP);
+
+	currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {
