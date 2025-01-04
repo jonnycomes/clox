1b42450c4c35de5a7557ffebb1d3464ed0f83266
Jonny Comes <jcomes@collegeofidaho.edu>
2025-01-04 09:23:24 -0700

    Adding 'this'.

--- src/compiler.c
+++ src/compiler.c
@@ -53,6 +53,7 @@ typedef struct {
 
 typedef enum {
 	TYPE_FUNCTION,
+	TYPE_METHOD,
 	TYPE_SCRIPT
 } FunctionType;
 
@@ -199,8 +200,15 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
 
 	Local* local = &current->locals[current->localCount++];
 	local->depth = 0;
-	local->name.start = "";
-	local->name.length = 0;
+	local->isCaptured = false;
+	if (type != TYPE_FUNCTION) {
+		local->name.start = "this";
+		local->name.length = 4;
+	} else {
+		local->name.start = "";
+		local->name.length = 0;
+	}
+
 }
 
 static ObjFunction* endCompiler() {
@@ -475,6 +483,10 @@ static void variable(bool canAssign) {
 	namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+	variable(false);
+}
+
 static void unary(bool canAssign) {
 	TokenType operatorType = parser.previous.type;
 
@@ -524,7 +536,7 @@ ParseRule rules[] = {
 	[TOKEN_PRINT]		  =	{NULL,     NULL,    PREC_NONE},
 	[TOKEN_RETURN]		  =	{NULL,     NULL,    PREC_NONE},
 	[TOKEN_SUPER]		  =	{NULL,     NULL,    PREC_NONE},
-	[TOKEN_THIS]		  =	{NULL,     NULL,    PREC_NONE},
+	[TOKEN_THIS]		  =	{this_,    NULL,    PREC_NONE},
 	[TOKEN_TRUE]		  =	{literal,  NULL,    PREC_NONE},
 	[TOKEN_VAR]			  =	{NULL,     NULL,    PREC_NONE},
 	[TOKEN_WHILE]		  =	{NULL,     NULL,    PREC_NONE},
@@ -602,7 +614,7 @@ static void method() {
 	consume(TOKEN_IDENTIFIER, "Expect method name");
 	uint8_t constant = identifierConstant(&parser.previous);
 
-	FunctionType type = TYPE_FUNCTION;
+	FunctionType type = TYPE_METHOD;
 	function(type);
 	emitBytes(OP_METHOD, constant);
 }
